41f022ea2695545b5695d35af03b07d7690e89c7
kubek22 <01171264@pw.edu.pl>
2024-02-22 13:16:13 +0100

    changing model

--- model.py
+++ model.py
@@ -1,39 +1,96 @@
 import numpy as np
 
+class Layer:
+    def __init__(self, function, weights):
+        """
+        weights as matrix
+        one function for all layer
+        """
 
-class Neuron:
-    def __init__(self, function, descendants=None, weights=None):
-        self.function = function
-        self.descendants = descendants  # list
-        self.weights = weights  # incoming
+        if type(weights) is list:
+            weights = np.array(weights)
 
-    def set_weights(self, weights):
         self.weights = weights
 
+        n_neurons = self.get_n_neurons()
+
+        self.function = function
+        # possible
+        self.functions = [function for i in range(n_neurons)]
+
     def compute(self, args):
-        # use weights and given function to produce the output (and activate descendants to compute)
-        return self.function(self.weights, args)
+        if self.weights.shape[1] != args.shape[0]:
+            return None
+        result = np.matmul(self.weights, args)
+        #if type(self.functions) is list: ...
+        return np.vectorize(self.function)(result)
 
+    def get_n_neurons(self):
+        return self.weights.shape[0]
 
-class Layer:
-    def __init__(self, n_neurons, function, descendants, weights):
-        self.n_neurons = n_neurons
-        # initialize net
-        self.neurons = [None for i in range(n_neurons)]
-        for i in range(n_neurons):
-            self.neurons[i] = Neuron(function, descendants, weights[i, :])
+    def get_weights(self):
+        return self.weights
+
+    def get_weight(self, neuron_index):
+        return self.weights[neuron_index] if neuron_index < self.get_n_neurons() else None
+
+    def get_functions(self):
+        return self.functions
+
+    def get_function(self, neuron_index):
+        return self.functions[neuron_index] if neuron_index < self.get_n_neurons() else None
 
     def set_weights(self, weights):
         pass
 
-    def compute(self, args):
-        pass
+
 
 
 class Net:
     # layers computing
-    def __init__(self, neurons_in_layers, weights):
-        self.n_layers = len(neurons_in_layers)
-        self.neurons_in_layers = neurons_in_layers  # ex. [1, 2, 2]
-        self.weights = weights
-        # creating layers starting from bottom
+    def __init__(self, weights, functions):
+        """
+        weights - list of matrices [layer, node, ancestor]
+        functions - list of functions, one per layer
+        """
+
+        n_layers = weights.shape[0]
+
+        if len(functions) == 1:
+            functions = [functions for i in range(n_layers)]
+
+        # creating layers
+        self.layers = [Layer(functions[i], weights) for i in range(n_layers)]
+
+    def compute(self, args):
+        for layer in self.layers:
+            args = layer.compute(args)
+            if args is None:
+                return None
+        return args
+
+    def get_n_layers(self):
+        return len(self.layers)
+
+    def get_all_weights(self):
+        return [layer.get_weights() for layer in self.layers]
+
+    def get_layer_weights(self, layer_index):
+        return self.layers[layer_index] if layer_index < self.get_n_layers() else None
+
+    def get_weight(self, layer_index, neuron_index):
+        return self.layers[layer_index].get_weight(neuron_index) if layer_index < self.get_n_layers() else None
+
+    def get_all_functions(self):
+        return [layer.get_functions() for layer in self.layers]
+
+    def get_layer_functions(self, layer_index):
+        return self.layers[layer_index].get_functions() if layer_index < self.get_n_layers() else None
+
+    def get_function(self, layer_index, neuron_index):
+        return self.layers[layer_index].get_function(neuron_index) if layer_index < self.get_n_layers() else None
+
+
+
+
+
